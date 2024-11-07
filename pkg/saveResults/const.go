0479704ac8041e6f0b609da009a6a090ea4f2424
Leonardo Bispo <leonardobispo1000@gmail.com>
2024-11-06 21:08:40 -0300

    refactor: delete pkg folder

--- pkg/saveResults/const.go
+++
@@ -1,8 +0,0 @@
-package saveResults
-
-const (
-	FileName      = "cryptocoin.csv"
-	CoinMarketURL = "https://coinmarketcap.com/all/views/all/"
-	ClassCell     = ".cmc-table__cell--sort-by"
-	ClassColumn   = ".cmc-table__column-name"
-)
