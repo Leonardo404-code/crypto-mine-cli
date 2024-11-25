21c5f86f4d5f4e3205f784f5f0beb3103cc2df31
Leonardo Bispo <leonardobispo1000@gmail.com>
2024-11-24 21:00:56 -0300

    chore: remove unused files

--- cmd/commands/saveResults/const.go
+++
@@ -1,9 +0,0 @@
-package saveResults
-
-const (
-	CSVFileName   = "cryptocoin.csv"
-	JSONFileName  = "cryptocoin.json"
-	CoinMarketURL = "https://coinmarketcap.com/all/views/all/"
-	ClassCell     = ".cmc-table__cell--sort-by"
-	ClassColumn   = ".cmc-table__column-name"
-)
